5f262d9c20f14562ac45a253046105a5c69d91d4
Novel <jungmini0601@gmail.com>
2025-12-21 02:43:00 +0900

    Title: 합과 평균, Time: 62ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251221/합과 평균/sum-and-avg.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+let [a, b] = fs.readFileSync(0)
+               .toString()
+               .split(" ")
+               .map(Number);
+
+let sum = a + b;
+let avg = sum / 2;
+
+console.log(sum, avg.toFixed(1));
