538b21f8a58658d0d342691b0260a45fd8ade428
Novel <jungmini0601@gmail.com>
2025-12-21 02:44:43 +0900

    Title: 합과 평균의 차, Time: 66ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251221/합과 평균의 차/sub-of-average-and-sum.js
@@ -0,0 +1,13 @@
+const fs = require('fs')
+let [a, b, c] = fs.readFileSync(0)
+                  .toString()
+                  .split(" ")
+                  .map(Number);
+
+let sum = a + b + c;
+let avg = sum / 3;
+let temp = sum - avg;
+
+console.log(sum);
+console.log(avg);
+console.log(temp);
