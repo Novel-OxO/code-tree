b3bc5b1e29ee5afd1b8c5172ba617ea96c1f0bc4
Novel <jungmini0601@gmail.com>
2025-12-24 21:03:30 +0900

    Title: 윤년인가, Time: 66ms, Memory: 10MB, Status: Passed - Codetree

--- 251224/윤년인가/is-leap-year.js
+++ 251224/윤년인가/is-leap-year.js
@@ -1,8 +1,8 @@
-const fs = require('fs');
+const fs = require("fs");
 const year = Number(fs.readFileSync(0).toString().trim());
 
-if(year % 4 === 0 || (year % 100 !== 0 && year % 400 === 0)) {
-    console.log('true');
+if (year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0)) {
+  console.log("true");
 } else {
-    console.log('false');
+  console.log("false");
 }
