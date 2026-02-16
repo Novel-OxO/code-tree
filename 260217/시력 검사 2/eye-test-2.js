258e08c19c17ca4f1ee681be2173968945b0cf92
Novel <jungmini0601@gmail.com>
2026-02-17 08:31:19 +0900

    Title: 시력 검사 2, Time: 65ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 260217/시력 검사 2/eye-test-2.js
@@ -0,0 +1,18 @@
+function input() {
+  const fs = require("fs");
+  const input = fs.readFileSync(0).toString().trim();
+  return Number(input);
+}
+
+function main() {
+  const t = input();
+  if (t >= 1) {
+    console.log("High");
+  } else if (t >= 0.5) {
+    console.log("Medium");
+  } else {
+    console.log("Low");
+  }
+}
+
+main();
