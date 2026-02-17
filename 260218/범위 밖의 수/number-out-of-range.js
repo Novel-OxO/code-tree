e89da0eb65129b6ed1af2b572b0b94394cdff64c
Novel <jungmini0601@gmail.com>
2026-02-18 07:21:26 +0900

    Title: 범위 밖의 수, Time: 69ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 260218/범위 밖의 수/number-out-of-range.js
@@ -0,0 +1,13 @@
+function input() {
+  const fs = require("fs");
+  let a = fs.readFileSync(0).toString().trim();
+  return Number(a);
+}
+
+function main() {
+  let a = input();
+
+  console.log(a < 10 || a > 20 ? "yes" : "no");
+}
+
+main();
