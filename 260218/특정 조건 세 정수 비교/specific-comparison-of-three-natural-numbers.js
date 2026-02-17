79ac1d7ac9eac6a071b04be410530d9ffc686f40
Novel <jungmini0601@gmail.com>
2026-02-18 07:09:30 +0900

    Title: 특정 조건 세 정수 비교, Time: 69ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 260218/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.js
@@ -0,0 +1,19 @@
+function input() {
+  const fs = require("fs");
+  let arr = fs.readFileSync(0).toString().trim().split(" ").map(Number);
+  let first = arr[0];
+  arr.sort();
+
+  return [first, arr];
+}
+
+function main() {
+  let [first, arr] = input();
+
+  let result1 = first === arr[0] ? 1 : 0;
+  let result2 = arr[0] === arr[2] ? 1 : 0;
+
+  console.log(`${result1} ${result2}`);
+}
+
+main();
