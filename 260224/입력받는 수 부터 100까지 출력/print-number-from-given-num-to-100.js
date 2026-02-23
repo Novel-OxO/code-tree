f2dddcb180c4dab34c8539891257c1080dab710d
Novel <jungmini0601@gmail.com>
2026-02-24 05:40:15 +0900

    Title: 입력받는 수 부터 100까지 출력, Time: 63ms, Memory: 10MB, Status: Runtime Error - Codetree

---
+++ 260224/입력받는 수 부터 100까지 출력/print-number-from-given-num-to-100.js
@@ -0,0 +1,17 @@
+function input() {
+  const fs = require("fs");
+  let inputs = fs.readFileSync(0).toString().trim();
+  return Number(inputs);
+}
+
+function main() {
+  let n = input();
+
+  for (let i = n; i <= 100; i++) {
+    result += i + " ";
+  }
+
+  console.log(result);
+}
+
+main();
