98bd7555e48a51539e57ea3682ae371f8f32661c
Novel <jungmini0601@gmail.com>
2026-02-25 05:37:56 +0900

    Title: B부터 A까지 감소, Time: 63ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 260225/B부터 A까지 감소/dec-from-b-to-a.js
@@ -0,0 +1,18 @@
+function input() {
+  const fs = require("fs");
+  let inputs = fs.readFileSync(0).toString().trim().split(' ');
+  return inputs.map(Number);
+}
+
+function main() {
+  let [a, b] = input();
+  let result = "";
+
+  for (let i = b; i >= a; i--) {
+    result += i + " ";
+  }
+
+  console.log(result);
+}
+
+main();
