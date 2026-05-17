e2a06f78e7de0a737114e5e5b8a6a890a5850c21
Novel <jungmini0601@gmail.com>
2026-05-18 05:21:03 +0900

    [Passed] 특정 수 맞히기 (67ms, 10MB)

---
+++ trail1/특정 수 맞히기/catching-specific-number.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+const path = require('path');
+
+function readInput() {
+    const localPath = path.join(__dirname, 'input.txt');
+    if(fs.existsSync(localPath)) {
+        return fs.readFileSync(localPath, 'utf8');
+    }
+    return fs.readFileSync(0, 'utf8');
+}
+
+function solve(raw) {
+    let numbers = raw.trim().split('\n').map(Number);
+
+    for(let number of numbers) {
+        if(number === 25) {
+            console.log('Good');
+            break;
+        }
+
+        if(number > 25) {
+            console.log('Lower');
+        } else {
+            console.log('Higher');
+        }
+    }
+}
+
+solve(readInput());
