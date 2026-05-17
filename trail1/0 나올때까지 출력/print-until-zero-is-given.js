f830ded12ba2033dee50a65421dfa90ee2ecede9
Novel <jungmini0601@gmail.com>
2026-05-17 13:13:12 +0900

    [Passed] 0 나올때까지 출력 (64ms, 10MB)

---
+++ trail1/0 나올때까지 출력/print-until-zero-is-given.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const path = require('path');
+
+function readInput() {
+    const localPath = path.join(__dirname, 'input.txt');
+    if(fs.existsSync(localPath)) {
+        return fs.readFileSync(localPath, 'utf8');
+    }
+    return fs.readFileSync(0, 'utf8');
+}
+
+function solve(raw) {
+    let numbers = raw.trim().split('\n').map(Number);
+
+    for(let number of numbers) {
+        if(number === 0)
+            break;
+        
+        console.log(number);
+    }
+}
+
+solve(readInput());
