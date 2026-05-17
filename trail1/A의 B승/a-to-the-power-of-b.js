a85bde4fda8f777132759c5852041d982c9ae57e
Novel <jungmini0601@gmail.com>
2026-05-17 12:32:42 +0900

    [Passed] A의 B승 (66ms, 10MB)

---
+++ trail1/A의 B승/a-to-the-power-of-b.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const path = require('path');
+
+function readInput() {
+    const localPath = path.join(__dirname, 'input.txt');
+    if(fs.existsSync(localPath)) {
+        return fs.readFileSync(localPath, 'utf8');
+    }
+    return fs.readFileSync(0, 'utf8');
+}
+
+function solve(raw) {
+    const [a, b] = raw.trim().split(' ').map(Number);
+    let result = 1;
+
+    for(let i = 1; i <= b; i++) {
+        result *= a;
+    }
+
+    console.log(result);
+}
+
+solve(readInput());
