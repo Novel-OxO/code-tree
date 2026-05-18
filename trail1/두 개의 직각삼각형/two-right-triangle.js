6eae9b5f2e8115262e4f76e6c0313ec9e5e248e9
Novel <jungmini0601@gmail.com>
2026-05-19 05:51:58 +0900

    [Passed] 두 개의 직각삼각형 (62ms, 10MB)

---
+++ trail1/두 개의 직각삼각형/two-right-triangle.js
@@ -0,0 +1,36 @@
+const fs = require('fs');
+const path = require('path');
+
+function readInput() {
+    const localPath = path.join(__dirname, 'input.txt');
+    if(fs.existsSync(localPath)) {
+        return fs.readFileSync(localPath, 'utf8');
+    }
+    return fs.readFileSync(0, 'utf8');
+}
+
+function solve(raw) {
+    let n = Number(raw.trim());
+
+    for(let i = 1; i <= n; i++) {
+        let stars = n - i + 1;
+        let spaces = i - 1;
+        let temp = '';
+        
+        for(let j = 1; j <= stars; j++) {
+            temp += '*';
+        }
+        for(let j = 1; j <= spaces; j++) {
+            temp += ' ';
+        }
+        for(let j = 1; j <= spaces; j++) {
+            temp += ' ';
+        }
+        for(let j = 1; j <= stars; j++) {
+            temp += '*';
+        }
+        console.log(temp);
+    }
+}
+
+solve(readInput());
