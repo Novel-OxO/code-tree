490891f2f3f7ab0bb57d9a3d426609db85fc681e
Novel <jungmini0601@gmail.com>
2026-05-19 05:13:21 +0900

    [Passed] 정사각형 별표 출력하기 (65ms, 10MB)

---
+++ trail1/정사각형 별표 출력하기/print-stars-in-square.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+const path = require('path');
+
+function readInput() {
+    const localPath = path.join(__dirname, 'input.txt');
+    if(fs.existsSync(localPath)) {
+        return fs.readFileSync(localPath, 'utf8');
+    }
+    return fs.readFileSync(0, 'utf8');
+}
+
+function solve(raw) {
+    let n = Number(raw.trim());
+
+    for(let i = 0; i < n; i++) {
+        let temp = '';
+        for(let j = 0; j < n; j++) {
+            temp += '*';
+        }
+        console.log(temp);
+    }
+}
+
+
+
+solve(readInput());
