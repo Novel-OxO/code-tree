6f0468fa4d292fceac35fc7cdaa1e03e81be4e61
Novel <jungmini0601@gmail.com>
2026-05-20 05:43:21 +0900

    [Passed] 별표 출력하기 8 (69ms, 10MB)

---
+++ trail1/별표 출력하기 8/print-star-8.js
@@ -0,0 +1,32 @@
+const fs = require('fs');
+const path = require('path');
+
+function readInput() {
+    const localPath = path.join(__dirname, 'input.txt');
+    if(fs.existsSync(localPath)) {
+        return fs.readFileSync(localPath, 'utf8');
+    }
+    return fs.readFileSync(0, 'utf8');
+}
+
+function solve(raw) {
+    let n = Number(raw.trim());
+
+    for(let i = 1; i <= n; i++) {
+        if(i % 2 === 1) {
+            console.log('*');
+            continue;
+        }
+
+        let stars = i;
+        let temp = '';
+        
+        for(let j = 1; j <= stars; j++) {
+            
+            temp += '* ';
+        }
+        console.log(temp);
+    }
+}
+
+solve(readInput());
