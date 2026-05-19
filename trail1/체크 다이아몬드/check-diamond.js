b6a762830da4c376d88c4102fe95836751679b8d
Novel <jungmini0601@gmail.com>
2026-05-20 05:39:53 +0900

    [Passed] 체크 다이아몬드 (64ms, 10MB)

---
+++ trail1/체크 다이아몬드/check-diamond.js
@@ -0,0 +1,44 @@
+const fs = require('fs');
+const path = require('path');
+
+function readInput() {
+    const localPath = path.join(__dirname, 'input.txt');
+    if(fs.existsSync(localPath)) {
+        return fs.readFileSync(localPath, 'utf8');
+    }
+    return fs.readFileSync(0, 'utf8');
+}
+
+function solve(raw) {
+    let n = Number(raw.trim());
+
+    for(let i = 1; i <= n; i++) {
+        let temp = '';
+        let spaces = n - i;
+        let stars = i;
+
+        for(let j = 1; j <= spaces; j++) {
+            temp += ' ';
+        }
+        for(let j = 1; j <= stars; j++) {
+            temp += '* ';
+        }
+        console.log(temp);
+    }
+
+    for(let i = n - 1; i >= 1; i--) {
+        let temp = '';
+        let spaces = n - i;
+        let stars = i;
+
+        for(let j = 1; j <= spaces; j++) {
+            temp += ' ';
+        }
+        for(let j = 1; j <= stars; j++) {
+            temp += '* ';
+        }
+        console.log(temp);
+    }
+}
+
+solve(readInput());
