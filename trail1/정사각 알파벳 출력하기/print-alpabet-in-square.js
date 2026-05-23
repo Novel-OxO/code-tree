31e4e6295580443c59fe986255475661a8ada1d7
Novel <jungmini0601@gmail.com>
2026-05-23 11:38:56 +0900

    [Passed] 정사각 알파벳 출력하기 (71ms, 14MB)

---
+++ trail1/정사각 알파벳 출력하기/print-alpabet-in-square.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+const path = require('path');
+
+function readInput() {
+    const localPath = path.join(__dirname, 'input.txt');
+    if(fs.existsSync(localPath)) {
+        return fs.readFileSync(localPath, 'utf8');
+    }
+    return fs.readFileSync(0, 'utf8');
+}
+
+
+function solve(raw) {
+    let n = Number(raw.trim());
+    let cnt = 'A'.charCodeAt(0);
+
+    for(let i = 1; i <= n; i++) {
+        let temp = '';
+        for(let j = 1; j <= n; j++) {
+            temp += String.fromCharCode(cnt);
+            cnt++;
+        }
+        console.log(temp);
+    }
+}
+
+solve(readInput());
