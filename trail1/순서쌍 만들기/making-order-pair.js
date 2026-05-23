e2d6fec323b285d6d565efbd3ab96790d8563706
Novel <jungmini0601@gmail.com>
2026-05-23 09:05:09 +0900

    [Passed] 순서쌍 만들기 (69ms, 10MB)

---
+++ trail1/순서쌍 만들기/making-order-pair.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+const path = require('path');
+
+function readInput() {
+    const localPath = path.join(__dirname, 'input.txt');
+    if(fs.existsSync(localPath)) {
+        return fs.readFileSync(localPath, 'utf8');
+    }
+    return fs.readFileSync(0, 'utf8');
+}
+
+
+function solve(raw) {
+    let n = Number(raw.trim());
+    
+    for(let i = n; i >= 1; i--) {
+        let temp = '';
+        
+        for(let j = n; j >= 1; j--) {
+            temp += `(${i},${j}) `;
+        }
+
+        console.log(temp);
+    }
+}
+
+solve(readInput());
