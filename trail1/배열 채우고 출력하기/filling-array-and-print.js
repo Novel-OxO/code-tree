13eafc4ca3922d77891b483828cc2e3d09a20301
Novel <jungmini0601@gmail.com>
2026-05-24 06:42:01 +0900

    [Passed] 배열 채우고 출력하기 (79ms, 30MB)

---
+++ trail1/배열 채우고 출력하기/filling-array-and-print.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+const path = require('path');
+
+function readInput() {
+    const localPath = path.join(__dirname, 'input.txt');
+    if(fs.existsSync(localPath)) {
+        return fs.readFileSync(localPath, 'utf8');
+    }
+    return fs.readFileSync(0, 'utf8');
+}
+
+
+function solve(raw) {
+    let arr = raw.trim().split(' ');
+    arr = arr.reverse();
+    console.log(arr.join(''));
+}
+
+solve(readInput());
