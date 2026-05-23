7871a77297f6887b1e65f078bfebe30033913614
Novel <jungmini0601@gmail.com>
2026-05-23 09:10:39 +0900

    [Passed] 좌우반전시킨 정사각형 (63ms, 10MB)

---
+++ trail1/좌우반전시킨 정사각형/left-right-inverted-square.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+const path = require('path');
+
+function readInput() {
+    const localPath = path.join(__dirname, 'input.txt');
+    if(fs.existsSync(localPath)) {
+        return fs.readFileSync(localPath, 'utf8');
+    }
+    return fs.readFileSync(0, 'utf8');
+}
+
+
+function solve(raw) {
+    let n = Number(raw.trim());
+
+    for(let i = 1; i <= n; i++) {
+        let temp = '';
+        for(let j = n; j >= 1; j--) {
+            temp += (i * j) + ' ';
+        }
+        console.log(temp);
+    }
+}
+
+solve(readInput());
