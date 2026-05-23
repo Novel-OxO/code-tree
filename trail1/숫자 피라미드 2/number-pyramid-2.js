14c916f43dbaf9f95856549d57c01f970e9f0686
Novel <jungmini0601@gmail.com>
2026-05-23 11:23:48 +0900

    [Passed] 숫자 피라미드 2 (64ms, 11MB)

---
+++ trail1/숫자 피라미드 2/number-pyramid-2.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+const path = require('path');
+
+function readInput() {
+    const localPath = path.join(__dirname, 'input.txt');
+    if(fs.existsSync(localPath)) {
+        return fs.readFileSync(localPath, 'utf8');
+    }
+    return fs.readFileSync(0, 'utf8');
+}
+
+
+function solve(raw) {
+    let n = Number(raw.trim());
+    let cnt = 1;
+
+    for(let i = 1; i <= n; i++) {
+        let temp = '';
+
+        for(let j = 1; j <= i; j++) {
+            temp += cnt + ' ';
+            cnt++;
+        }
+        console.log(temp);
+    }
+}
+
+solve(readInput());
