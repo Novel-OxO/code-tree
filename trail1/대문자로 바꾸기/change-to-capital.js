4e31413be352961270ae5f7beffc5505aa3dff6d
Novel <jungmini0601@gmail.com>
2026-05-27 07:09:29 +0900

    [Passed] 대문자로 바꾸기 (68ms, 12MB)

---
+++ trail1/대문자로 바꾸기/change-to-capital.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const path = require('path');
+
+function readInput() {
+    const localPath = path.join(__dirname, 'input.txt');
+    if(fs.existsSync(localPath)) {
+        return fs.readFileSync(localPath, 'utf8');
+    }
+    return fs.readFileSync(0, 'utf8');
+}
+// N개의 정수가 주어졌을 때, 중복하여 등장하지 않는 정수 중 최댓값을 구하는 프로그램을 작성해보세요.
+function solve(raw) {
+    const diff = 'A'.charCodeAt(0) - 'a'.charCodeAt(0);
+    const arr2D = raw.trim().split('\n').map(line => line.split(' ').map(x => x.charCodeAt(0) + diff).map(x => String.fromCharCode(x).toString()));
+    
+    for(let arr of arr2D) {
+        console.log(arr.join(' '));
+    }
+}
+
+solve(readInput());
