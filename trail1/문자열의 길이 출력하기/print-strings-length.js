06948a40c60b8b55b6098170fff3ae1b8b97b8a3
Novel <jungmini0601@gmail.com>
2026-05-30 09:44:23 +0900

    [Passed] 문자열의 길이 출력하기 (65ms, 10MB)

---
+++ trail1/문자열의 길이 출력하기/print-strings-length.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+const path = require('path');
+
+function readInput() {
+    const localPath = path.join(__dirname, 'input.txt');
+    if(fs.existsSync(localPath)) {
+        return fs.readFileSync(localPath, 'utf8');
+    }
+    return fs.readFileSync(0, 'utf8');
+}
+
+function solve(raw) {
+    let [a, b] = raw.trim().split('\n');
+    console.log(a.length + b.length);
+}
+
+solve(readInput());
