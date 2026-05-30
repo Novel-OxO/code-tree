34440146b1fd61c5cd5fe3190fc2e6c8b324813e
Novel <jungmini0601@gmail.com>
2026-05-30 09:43:40 +0900

    [Passed] 문자열 두번 출력하기 (73ms, 16MB)

---
+++ trail1/문자열 두번 출력하기/print-string-twice.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+const path = require('path');
+
+function readInput() {
+    const localPath = path.join(__dirname, 'input.txt');
+    if(fs.existsSync(localPath)) {
+        return fs.readFileSync(localPath, 'utf8');
+    }
+    return fs.readFileSync(0, 'utf8');
+}
+
+function solve(raw) {
+    let input = raw.trim();
+    console.log(input);
+    console.log(input);
+}
+
+solve(readInput());
