70671e8e5b43316cf6786ec226ef9d77c02d2d45
Novel <jungmini0601@gmail.com>
2026-05-30 09:42:37 +0900

    [Passed] 격자 모양 출력하기 (63ms, 10MB)

---
+++ trail1/격자 모양 출력하기/print-grid-shape.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+const path = require('path');
+
+function readInput() {
+    const localPath = path.join(__dirname, 'input.txt');
+    if(fs.existsSync(localPath)) {
+        return fs.readFileSync(localPath, 'utf8');
+    }
+    return fs.readFileSync(0, 'utf8');
+}
+
+function solve(raw) {
+    let lines = raw.trim().split('\n');
+    let [n ,m] = lines[0].split(' ').map(Number);
+    let coins = lines.slice(1).map(line => line.split(' ').map(Number));
+    let arr = new Array(n + 1).fill(0).map(() => new Array(n + 1).fill(0));
+
+    for(let [x ,y] of coins) {
+        arr[x][y] = x * y;
+    }
+
+    let temp = '';
+    for(let i = 1; i <= n; i++) {
+        for(let j = 1; j <= n; j++) {
+            temp += arr[i][j] + ' ';
+        }
+        temp += '\n';
+    }
+
+    console.log(temp);
+}
+
+solve(readInput());
