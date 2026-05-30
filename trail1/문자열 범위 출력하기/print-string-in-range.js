dfd79cde20d192bcf614fff135cf0272c90e262e
Novel <jungmini0601@gmail.com>
2026-05-30 09:49:45 +0900

    [Passed] 문자열 범위 출력하기 (64ms, 10MB)

---
+++ trail1/문자열 범위 출력하기/print-string-in-range.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+const path = require('path');
+
+function readInput() {
+    const localPath = path.join(__dirname, 'input.txt');
+    if(fs.existsSync(localPath)) {
+        return fs.readFileSync(localPath, 'utf8');
+    }
+    return fs.readFileSync(0, 'utf8');
+}
+
+function solve(raw) {
+    let input = raw.trim();
+    console.log(input.slice(2, 10));
+}
+
+solve(readInput());
