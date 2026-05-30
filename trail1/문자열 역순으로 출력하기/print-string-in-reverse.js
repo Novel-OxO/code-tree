d7775ddfd2503c8af4e7ce728cf2c80d0bf9dad3
Novel <jungmini0601@gmail.com>
2026-05-30 10:07:45 +0900

    [Passed] 문자열 역순으로 출력하기 (61ms, 10MB)

---
+++ trail1/문자열 역순으로 출력하기/print-string-in-reverse.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+const path = require('path');
+
+function readInput() {
+    const localPath = path.join(__dirname, 'input.txt');
+    if(fs.existsSync(localPath)) {
+        return fs.readFileSync(localPath, 'utf8');
+    }
+    return fs.readFileSync(0, 'utf8');
+}
+
+function solve(raw) {
+    const input = raw.trim().split('\n');
+    const arr = input.reverse();
+    console.log(arr.join('\n'));
+}
+
+solve(readInput());
