03973e77311094e39ff912cb37c8da8001276405
Novel <jungmini0601@gmail.com>
2026-05-31 18:56:18 +0900

    [Passed] 첫 번째와 두 번째 교환 (61ms, 10MB)

---
+++ trail1/첫 번째와 두 번째 교환/exchange-1st-and-2nd.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+const path = require('path');
+
+function readInput() {
+    const localPath = path.join(__dirname, 'input.txt');
+    if(fs.existsSync(localPath)) {
+        return fs.readFileSync(localPath, 'utf8');
+    }
+    return fs.readFileSync(0, 'utf8');
+}
+
+function solve(raw) {
+    let str = raw.trim().split('');
+    let first = str[0];
+    let second = str[1];
+
+    for(let i = 0; i < str.length; i++) {
+        if(str[i] === first) {
+            str[i] = second;
+            continue;
+        }
+
+        if(str[i] === second) {
+            str[i] = first;
+        }
+    }
+
+    console.log(str.join(''));
+}
+
+solve(readInput());
