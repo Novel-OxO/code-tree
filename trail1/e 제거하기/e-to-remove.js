01c897a89090c0f547345bc32e58c6240c1e0139
Novel <jungmini0601@gmail.com>
2026-05-31 19:31:15 +0900

    [Passed] e 제거하기 (67ms, 10MB)

---
+++ trail1/e 제거하기/e-to-remove.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const path = require('path');
+
+function readInput() {
+    const localPath = path.join(__dirname, 'input.txt');
+    if(fs.existsSync(localPath)) {
+        return fs.readFileSync(localPath, 'utf8');
+    }
+    return fs.readFileSync(0, 'utf8');
+}
+
+function solve(raw) {
+    let str = raw.trim();
+    str = str.split('');
+    let idx = str.indexOf('e');
+    
+    str[idx] = '';
+    console.log(str.join(''));
+}
+
+solve(readInput());
